51336e27b2558f84b664b28a33055bbaec342345
jkmmo <31679886+jkmmo@users.noreply.github.com>
2023-12-29 15:27:52 -0500

    Update v.2
    now supports more than one file

--- dockerfile
+++ dockerfile
@@ -9,6 +9,7 @@ RUN npm install
 COPY . .
 
 ENV PORT = 8081
+
 EXPOSE  8081
 
 CMD ["npm","start"]
--- server.js
+++ server.js
@@ -2,12 +2,10 @@ const express = require('express');
 const sharp = require('sharp');
 const multer = require('multer');
 const path = require('path');
-const fs = require("fs");
 const { env } = require('process');
 
-const Port = env.TESTPORT || 8080
 const app = express();
-const port = Port; // Update the port to env port or 8080
+const port = env.PORT || 8080; // Default to port 8080 if env.PORT is not defined
 
 // Define a storage location for uploaded files
 const storage = multer.memoryStorage();
@@ -18,33 +16,49 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+// ...
+
 // Handle file upload and metadata stripping
-app.post('/strip-metadata', upload.single('image'), async (req, res) => {
+app.post('/strip-metadata', upload.array('image[]'), async (req, res) => {
     try {
-        if (!req.file) {
-            return res.status(400).send('No file uploaded.');
+        if (!req.files || req.files.length === 0) {
+            return res.status(400).send('No files uploaded.');
         }
-        const imageBuffer = req.file.buffer;
 
-        // Strip Exif metadata using sharp
-        const strippedImageBuffer = await sharp(imageBuffer).jpeg().toBuffer();
-        const NewPng = await sharp(strippedImageBuffer.buffer).png().toBuffer()
+        const strippedImageUrls = [];
 
-        // Set the response content type to image/jpeg
-        res.setHeader('Content-Type', 'image/jpeg');
+        for (const file of req.files) {
+            const imageBuffer = file.buffer;
 
-        // Send the stripped image as a response
-        res.send(NewPng);
+            // Strip Exif metadata using sharp
+            const strippedImageBuffer = await sharp(imageBuffer).jpeg().toBuffer();
+            const newPNG = await sharp(strippedImageBuffer).png().toBuffer();
+
+            // Save the stripped image to a temporary file or cloud storage
+            // Generate a URL for the saved image (e.g., using a CDN or local path)
+            // For example, you can save to a local path:
+            // const savedImagePath = `/path/to/saved/image/${uniqueIdentifier}.png`;
+            // fs.writeFileSync(savedImagePath, newPNG);
+
+            // Add the URL to the array
+            // strippedImageUrls.push(savedImagePath);
+
+            // For demonstration purposes, we'll use a data URL
+            const dataUrl = `data:image/png;base64,${newPNG.toString('base64')}`;
+            strippedImageUrls.push(dataUrl);
+        }
+
+        // Send the array of image URLs as a JSON response
+        res.json(strippedImageUrls);
     } catch (error) {
         console.error('Error stripping metadata:', error);
         res.status(500).send('Error stripping metadata');
     }
 });
 
-app.get('/get-port', (req, res) => {
-    res.json({ port: Port });
-});
+// ...
+
 
 app.listen(port, () => {
-    console.log(`Meta Remover running on http://localhost:${port}`);
+    console.log(`MetaData-Remover running at http://localhost:${port}`);
 });
