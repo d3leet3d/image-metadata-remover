92c30af6e34c3c53a553c089169b8f76d12a39ff
d3leet3d <31679886+d3leet3d@users.noreply.github.com>
2024-03-14 19:15:19 -0400

    Update server.js

--- server.js
+++ server.js
@@ -5,20 +5,17 @@ const path = require('path');
 const { env } = require('process');
 
 const app = express();
-const port = env.PORT || 8080; // Default to port 8080 if env.PORT is not defined
-
-// Define a storage location for uploaded files
+const port = env.PORT || 8080; 
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
-// Serve the HTML page (don't use express.static here)
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// ...
 
-// Handle file upload and metadata stripping
+
+
 app.post('/strip-metadata', upload.array('image[]'), async (req, res) => {
     try {
         if (!req.files || req.files.length === 0) {
@@ -33,22 +30,10 @@ app.post('/strip-metadata', upload.array('image[]'), async (req, res) => {
             // Strip Exif metadata using sharp
             const strippedImageBuffer = await sharp(imageBuffer).jpeg().toBuffer();
             const newPNG = await sharp(strippedImageBuffer).png().toBuffer();
-
-            // Save the stripped image to a temporary file or cloud storage
-            // Generate a URL for the saved image (e.g., using a CDN or local path)
-            // For example, you can save to a local path:
-            // const savedImagePath = `/path/to/saved/image/${uniqueIdentifier}.png`;
-            // fs.writeFileSync(savedImagePath, newPNG);
-
-            // Add the URL to the array
-            // strippedImageUrls.push(savedImagePath);
-
-            // For demonstration purposes, we'll use a data URL
             const dataUrl = `data:image/png;base64,${newPNG.toString('base64')}`;
             strippedImageUrls.push(dataUrl);
         }
-
-        // Send the array of image URLs as a JSON response
+        
         res.json(strippedImageUrls);
     } catch (error) {
         console.error('Error stripping metadata:', error);
